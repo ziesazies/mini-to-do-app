a05a4bbfcf48f9b1810cbed26107bfd507401d8b
Alief Azies <alief.ahmadazies@gmail.com>
2025-05-05 00:11:25 +0700

    refactor: disable debug check mark

--- lib/main.dart
+++ lib/main.dart
@@ -34,6 +34,7 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         ),
+        debugShowCheckedModeBanner: false,
         home: isLoggedIn ? const TodoListPage() : const LoginPage(),
         routes: {
           '/login': (_) => const LoginPage(),
