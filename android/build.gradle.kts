bb9eb68c264d8bfab03b2148803ec0930056c815
Alief Azies <alief.ahmadazies@gmail.com>
2025-05-05 00:10:17 +0700

    chore: slidable feature dependencies and android NDK for build APK

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,6 +5,11 @@ allprojects {
     }
 }
 
+android {
+        ndkVersion = "27.0.12077973"
+        ...
+    }
+
 val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
 rootProject.layout.buildDirectory.value(newBuildDir)
 
