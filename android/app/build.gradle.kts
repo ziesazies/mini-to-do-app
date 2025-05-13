01a72756fa67dfb5762ae89ec1395741b05a0012
Alief Azies <alief.ahmadazies@gmail.com>
2025-05-13 15:09:30 +0700

    chore: ndkVersion handling for sharedPreference and build APK purpose

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,9 +6,9 @@ plugins {
 }
 
 android {
+    ndkVersion = "27.0.12077973"
     namespace = "com.example.mini_to_do_app"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,11 +5,6 @@ allprojects {
     }
 }
 
-android {
-        ndkVersion = "27.0.12077973"
-        ...
-    }
-
 val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
 rootProject.layout.buildDirectory.value(newBuildDir)
 
